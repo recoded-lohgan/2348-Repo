62de3c87af0152f26f8ac018c1232983bc7d43f1
recoded-lohgan <112371914+recoded-lohgan@users.noreply.github.com>
2023-10-08 00:08:29 -0500

    Add files via upload

---
+++ HW2/zylab6_22ljoseph.py
@@ -0,0 +1,21 @@
+
+# Lohgan Joseph
+# 2038027
+# Place six user-input variables
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+e = int(input())
+f = int(input())
+
+solution_found = False
+
+for x in range(-10,11):
+    for y in range(-10,11):
+        if a * x + b * y == c and d * x + e * y == f:
+            print(x,y)
+            solution_found = True
+
+if not solution_found:
+    print("No solution")
